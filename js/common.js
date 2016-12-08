a87d5d6b58a9dd5bdeb729b7f8f7901d198c14c3
ashsavag <ashsavage@gmail.com>
2016-12-08 13:21:38 +0200

    alt

---
+++ js/common.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ash Savage on 02.12.2016.
+ */
+$
--- js/slider.js
+++ js/slider.js
@@ -5,7 +5,7 @@ var time = window.setInterval(function photoA() {
     imageCount = imageCount + 1;
     if(imageCount > total){imageCount = 1;}
     if(imageCount < 1){imageCount = total;}
-    image.src = "images/img"+ imageCount +".jpg";
+    image.src = "img/img"+ imageCount +".jpg";
 },2000);
 
 function photo(x) {
@@ -13,14 +13,14 @@ function photo(x) {
 	imageCount = imageCount + x;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
-	image.src = "images/img"+ imageCount +".jpg";
+	image.src = "img/img"+ imageCount +".jpg";
 	clearInterval(time);
 	time =  window.setInterval(function photoA() {
 	var image = document.getElementById('image');
 	imageCount = imageCount + 1;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
-	image.src = "images/img"+ imageCount +".jpg";
+	image.src = "img/img"+ imageCount +".jpg";
 	},2000);
 	}
  
